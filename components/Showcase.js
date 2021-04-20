b5feab98e4a44561fd2337c7da8a060633fbdf53
Daniel Silver <dabzbm@gmail.com>
2021-04-20 08:28:16 -0400

    Data Fetching

---
+++ components/Showcase.js
@@ -0,0 +1,12 @@
+import styles from '@/styles/Showcase.module.css';
+
+const Showcase = () => {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to the Party!</h1>
+      <h2>Find Local Live Music Events</h2>
+    </div>
+  );
+};
+
+export default Showcase;
---
+++ pages/404.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import { FaExclamationTriangle } from 'react-icons/fa';
+import Layout from '@/components/Layout';
+import styles from '@/styles/404.module.css';
+
+const NotFoundPage = () => {
+  return (
+    <Layout title='Page not Found'>
+      <div className={styles.error}>
+        <FaExclamationTriangle size={60} style={{ color: 'crimson' }} />
+        <h1>404</h1>
+        <h4>Sorry Page Not Found]</h4>
+        <Link href='/'>Home</Link>
+      </div>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
