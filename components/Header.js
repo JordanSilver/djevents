2e375526ebdbc5a8847946dffcf0975c89953fa9
Daniel Silver <dabzbm@gmail.com>
2021-04-22 10:44:21 -0400

    AUTH, JWT, COOKIES

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,12 @@
 import Link from 'next/link';
 import styles from '@/styles/Header.module.css';
 import Search from './Search';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import AuthContext from '@/context/AuthContext';
+import { useContext } from 'react';
 
 const Header = () => {
+  const { user, logout } = useContext(AuthContext);
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -18,11 +22,40 @@ const Header = () => {
               <a>Events</a>
             </Link>
           </li>
-          <li>
-            <Link href='/events/add'>
-              <a>Add Event</a>
-            </Link>
-          </li>
+          {user ? (
+            <>
+              <li>
+                <Link href='/events/add'>
+                  <a>Add Event</a>
+                </Link>
+              </li>
+              <li>
+                <Link href='/account/dashboard'>
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+
+              <li>
+                <button
+                  onClick={() => logout()}
+                  className='btn-secondary btn-icon'
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href='/account/login'>
+                  <a className='btn-secondary btn-icon'>
+                    {' '}
+                    <FaSignInAlt /> Log In
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
